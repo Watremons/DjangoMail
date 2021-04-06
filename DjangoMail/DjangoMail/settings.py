1f841b1a5f075b4aa383030cf64103cfbd225b72
bopipilala <73065824+bopipilala@users.noreply.github.com>
2021-04-06 15:50:41 +0800

    Update settings.py

--- DjangoMail/DjangoMail/settings.py
+++ DjangoMail/DjangoMail/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'WebMail',
 ]
 
 ROOT_URLCONF = 'DjangoMail.urls'
