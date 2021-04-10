1494f8ece7dd8da1408265c0f9497db795f558e4
wall <qc8808@gmail.com>
2021-04-11 01:20:18 +0800

    🐞fix(settings, views):
    修复settings中无法引入server的问题

--- DjangoMail/DjangoMail/settings.py
+++ DjangoMail/DjangoMail/settings.py
@@ -18,7 +18,8 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 from MailSystem.server import Server
 
-mailSystemServer = Server()
+TMP = 'good'
+# mailSystemServer = Server()
 
 mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
