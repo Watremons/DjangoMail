0be986b297c0e21a52a279dd8550a9b9d9d92457
Watremons <z2331355763@sina.com>
2021-05-14 23:07:05 +0800

    ✨ feat(mail-frontend): 修改前端页面
    修改前端日志管理页面，显示smtp日志和pop日志并实现单个查看，单个删除与全部删除功能

--- mail-frontend/src/components/LogManage.vue
+++ mail-frontend/src/components/LogManage.vue
@@ -7,112 +7,293 @@
         </div>
         <div class="container">
             <el-tabs v-model="message">
-                <el-tab-pane :label="`未读消息(${unread.length})`" name="first">
-                    <el-table :data="unread" :show-header="false" style="width: 100%">
+                <el-tab-pane :label="`SMTP日志(${smtpLogList.length})`" name="first">
+                    <el-table :data="smtpLogList" :show-header="false" style="width: 100%">
                         <el-table-column>
                             <template slot-scope="scope">
-                                <span class="message-title">{{scope.row.title}}</span>
+                                <span class="message-title" @click="showSmtpLog(scope.$index)">{{scope.row.logName}}</span>
                             </template>
                         </el-table-column>
-                        <el-table-column prop="date" width="180"></el-table-column>
+                        <el-table-column prop="logTime" width="180"></el-table-column>
                         <el-table-column width="120">
                             <template slot-scope="scope">
-                                <el-button size="small" @click="handleRead(scope.$index)">标为已读</el-button>
+                                <el-button type="danger" @click="handleDelSmtpLog(scope.$index)">删除</el-button>
                             </template>
                         </el-table-column>
                     </el-table>
                     <div class="handle-row">
-                        <el-button type="primary">全部标为已读</el-button>
+                        <el-button type="danger" @click="delAllSmtpLog()">删除全部</el-button>
                     </div>
                 </el-tab-pane>
-                <el-tab-pane :label="`已读消息(${read.length})`" name="second">
-                    <template v-if="message === 'second'">
-                        <el-table :data="read" :show-header="false" style="width: 100%">
-                            <el-table-column>
-                                <template slot-scope="scope">
-                                    <span class="message-title">{{scope.row.title}}</span>
-                                </template>
-                            </el-table-column>
-                            <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
-                                <template slot-scope="scope">
-                                    <el-button type="danger" @click="handleDel(scope.$index)">删除</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                        <div class="handle-row">
-                            <el-button type="danger">删除全部</el-button>
-                        </div>
-                    </template>
-                </el-tab-pane>
-                <el-tab-pane :label="`回收站(${recycle.length})`" name="third">
-                    <template v-if="message === 'third'">
-                        <el-table :data="recycle" :show-header="false" style="width: 100%">
-                            <el-table-column>
-                                <template slot-scope="scope">
-                                    <span class="message-title">{{scope.row.title}}</span>
-                                </template>
-                            </el-table-column>
-                            <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
-                                <template slot-scope="scope">
-                                    <el-button @click="handleRestore(scope.$index)">还原</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                        <div class="handle-row">
-                            <el-button type="danger">清空回收站</el-button>
-                        </div>
-                    </template>
+                <el-tab-pane :label="`POP3日志(${pop3LogList.length})`" name="second">
+                    <el-table :data="pop3LogList" :show-header="false" style="width: 100%">
+                        <el-table-column>
+                            <template slot-scope="scope">
+                                <span class="message-title" @click="showPop3Log(scope.$index)">{{scope.row.logName}}</span>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="logTime" width="150"></el-table-column>
+                        <el-table-column width="120">
+                            <template slot-scope="scope">
+                                <el-button type="danger" @click="handleDelPop3Log(scope.$index)">删除</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                    <div class="handle-row">
+                        <el-button type="danger" @click="delAllPop3Log()">删除全部</el-button>
+                    </div>
                 </el-tab-pane>
             </el-tabs>
         </div>
+        <!-- 新增信息弹出框 -->
+        <el-dialog :title="dialogTitle" :visible.sync="logVisible" width="50%">
+            <!-- <el-form
+                ref="form"
+                :model="addForm"
+                label-width="70px"
+                label-position="top"
+                @submit.native.prevent>
+                <el-form-item :label=dialogTitle>
+                    <el-input
+                        v-model="addForm.addInfo"
+                        clearable
+                        placeholder="请输入添加信息"
+                        minlength="7"
+                        maxlength="15"
+                        show-word-limit
+                        ></el-input>
+                </el-form-item>
+            </el-form>-->
+            <span style="white-space: pre-line">{{this.logContent}}</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="logVisible = false">关 闭</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
+import Axios from 'axios'
     export default {
         name: 'tabs',
         data() {
             return {
                 message: 'first',
-                showHeader: false,
-                unread: [{
-                    date: '2018-04-19 20:00:00',
-                    title: 'SMTP日志',
-                },{
-                    date: '2018-04-19 21:00:00',
-                    title: 'POP3日志',
-                }],
-                read: [{
-                    date: '2018-04-19 20:00:00',
-                    title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
-                }],
-                recycle: [{
-                    date: '2018-04-19 20:00:00',
-                    title: '【系统通知】该系统将于今晚凌晨2点到5点进行升级维护'
+                dialogTitle: "日志",
+                logVisible: false,
+                logContent: "",
+                smtpLogList: [{
+                    logName: 'SMTP日志',
+                    logTime: '2018-04-19 20:00:00',
+                    logContent: 'Nothing to log'
+                },],
+                pop3LogList: [,{
+                    logName: 'POP3日志',
+                    logTime: '2018-04-19 21:00:00',
+                    logContent: 'Nothing to log'
                 }]
             }
         },
-        methods: {
-            handleRead(index) {
-                const item = this.unread.splice(index, 1);
-                console.log(item);
-                this.read = item.concat(this.read);
-            },
-            handleDel(index) {
-                const item = this.read.splice(index, 1);
-                this.recycle = item.concat(this.recycle);
-            },
-            handleRestore(index) {
-                const item = this.recycle.splice(index, 1);
-                this.read = item.concat(this.read);
-            }
+        created() {
+            this.getAllLog()
         },
-        computed: {
-            unreadNum(){
-                return this.unread.length;
-            }
+        methods: {
+            // 获取所有日志文件
+            getAllLog() {
+                let requestURL = '/apis/webmail/getlog/';
+                let data = new FormData();
+                data.append("logType",0)
+                Axios
+                    .post(requestURL,data)
+                    .then(response => {
+                        // console.log(JSON.parse(response.data.data));
+                        this.smtpLogList = JSON.parse(response.data.data);
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: 'error',
+                            message: 'SMTP日志内容获取失败'
+                        })
+                    })
+                
+                let requestURL2 = '/apis/webmail/getlog/';
+                let data2 = new FormData();
+                data2.append("logType",1)
+                Axios
+                    .post(requestURL2,data2)
+                    .then(response => {
+                        console.log(JSON.parse(response.data.data));
+                        this.pop3LogList = JSON.parse(response.data.data)
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: 'error',
+                            message: 'POP3日志内容获取失败'
+                        })
+                    })
+            },
+            //展示smtp日志
+            showSmtpLog(index) {
+                this.logVisible = true;
+                this.logContent = this.smtpLogList[index].logContent;
+            },
+            // 展示pop3日志
+            showPop3Log(index) {
+                this.logVisible = true;
+                this.logContent = this.pop3LogList[index].logContent;
+            },
+            // 删除指定SMTP日志文件
+            handleDelSmtpLog(index) {
+                let requestURL = '/apis/webmail/dellog/';
+                let data = new FormData();
+                data.append("logType",0)
+
+                let logIndexList = [];
+                logIndexList.push(index + 1);
+
+                data.append("logIndexList",JSON.stringify(logIndexList))
+                Axios
+                    .post(requestURL,data)
+                    .then(response => {
+                        if (response.data.status == 200)
+                        {
+                            this.$message.success("删除成功");
+                        }
+                        else if (response.data.status == 201)
+                        {
+                            this.$message.error("删除失败");
+                        }
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: 'error',
+                            message: '指定日志删除失败'
+                        })
+                    })
+
+                setTimeout(() => {
+                    this.getAllLog()
+                }, 1000);
+            },
+            // 删除指定POP3日志文件
+            handleDelPop3Log(index) {
+                let requestURL = '/apis/webmail/dellog/';
+                let data = new FormData();
+                data.append("logType",1)
+
+                let logIndexList = [];
+                logIndexList.push(index + 1);
+
+                data.append("logIndexList",JSON.stringify(logIndexList))
+                console.log(JSON.stringify(logIndexList))
+                Axios
+                    .post(requestURL,data)
+                    .then(response => {
+                        if (response.data.status == 200)
+                        {
+                            this.$message.success("删除成功");
+                        }
+                        else if (response.data.status == 201)
+                        {
+                            this.$message.error("删除失败");
+                        }
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: 'error',
+                            message: '指定日志删除失败'
+                        })
+                    })
+
+                setTimeout(() => {
+                    this.getAllLog()
+                }, 1000);
+            },
+            // 删除所有SMTP日志文件
+            delAllSmtpLog() {
+                let requestURL = '/apis/webmail/dellog/';
+                let data = new FormData();
+                data.append("logType",0)
+
+                let logIndexList = [];
+                let length = this.smtpLogList.length;
+                for(let i=0;i<length;i++)
+                {
+                    logIndexList.push(i+1);
+                }
+                if(logIndexList.length == 0)
+                {
+                    this.$message.error("当前没有可删除的SMTP日志");
+                    return;
+                }
+
+                data.append("logIndexList",JSON.stringify(logIndexList))
+                Axios
+                    .post(requestURL,data)
+                    .then(response => {
+                        if (response.data.status == 200)
+                        {
+                            this.$message.success("全部删除成功");
+                        }
+                        else if (response.data.status == 201)
+                        {
+                            this.$message.error("存在部分删除失败");
+                        }
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: 'error',
+                            message: 'SMTP日志删除失败'
+                        })
+                    })
+
+                setTimeout(() => {
+                    this.getAllLog()
+                }, 1000);
+            },
+            // 删除所有POP3日志文件
+            delAllPop3Log() {
+                let requestURL = '/apis/webmail/dellog/';
+                let data = new FormData();
+                data.append("logType",1)
+
+                let logIndexList = [];
+                let length = this.pop3LogList.length;
+                for(let i=0;i<length;i++)
+                {
+                    logIndexList.push(i+1);
+                }
+
+                if(logIndexList.length == 0)
+                {
+                    this.$message.error("当前没有可删除的POP3日志");
+                    return;
+                }
+
+                data.append("logIndexList",JSON.stringify(logIndexList))
+                Axios
+                    .post(requestURL,data)
+                    .then(response => {
+                        if (response.data.status == 200)
+                        {
+                            this.$message.success("全部删除成功");
+                        }
+                        else if (response.data.status == 201)
+                        {
+                            this.$message.error("存在部分删除失败");
+                        }
+                    })
+                    .catch(() => {
+                        this.$message({
+                            type: 'error',
+                            message: 'POP3日志删除失败'
+                        })
+                    })
+
+                setTimeout(() => {
+                    this.getAllLog()
+                }, 1000);
+            },
         }
     }
 
