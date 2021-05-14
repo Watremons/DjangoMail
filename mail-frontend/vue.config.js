257be5a986bbf16c70e657c852799b958aff995b
Watremons <z2331355763@sina.com>
2021-05-14 23:04:10 +0800

    🎈 perf(vue.config.js): 修改vue配置

--- mail-frontend/vue.config.js
+++ mail-frontend/vue.config.js
@@ -3,6 +3,12 @@ module.exports = {
     assetsDir: 'static',
     productionSourceMap: false,
     devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        disableHostCheck:true,
+        open: true,
         proxy: {
             '/apis': {    
                 target: 'http://127.0.0.1:8000',  // 接口域名
